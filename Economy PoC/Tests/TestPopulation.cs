0d90ab503fbcbe7388159e98d9f1243ed3093a7e
robert staples <robertstaples@roberts-MacBook-Pro.local>
2018-03-29 14:04:02 +1100

    Finished adding the unit testing

--- Economy PoC/Tests/TestPopulation.cs
+++ Economy PoC/Tests/TestPopulation.cs
@@ -43,7 +43,7 @@ namespace Economy_PoC
             testPop.inventory.Add(new Stock(test1, 10));
             testPop.inventory.Add(new Stock(test2, 10));
 
-            //Call Funtion
+            //Call Function
             testPop.CalculateGrowthAndPopulation();
 
             //Asset Outcome
@@ -55,7 +55,7 @@ namespace Economy_PoC
 
         //Test 3 - Calculate Growth with two needs, and with inventory leftover
         [TestCase]
-        public void Test_Two_Needs_Many()
+        public void Test_Two_Needs_Leftover()
         {
             //Setup
             Commodity test1 = new Commodity("test1");
@@ -65,23 +65,41 @@ namespace Economy_PoC
             testNeed.Add(new Need(test2, 1));
             Population testPop = new Population(10, testNeed, 5);
             testPop.deltaSpeed = 5;
-            testPop.inventory.Add(new Stock(test1, 10));
-            testPop.inventory.Add(new Stock(test2, 10));
+            testPop.inventory.Add(new Stock(test1, 15));
+            testPop.inventory.Add(new Stock(test2, 15));
 
-            //Call Funtion
-
+            //Call Function
+            testPop.CalculateGrowthAndPopulation();
 
             //Asset Outcome
+            Assert.AreEqual(0.5, testPop.growth);
+
+            Assert.AreEqual(2, testPop.inventory.Count);
+
+
         }
         //Test 4 - Calculate Growth with two needs of different amount and with inventory leftover
         [TestCase]
         public void Test_Two_Needs_Multi()
         {
             //Setup
+            Commodity test1 = new Commodity("test1");
+            Commodity test2 = new Commodity("test2");
+            List<Need> testNeed = new List<Need>();
+            testNeed.Add(new Need(test1, 1));
+            testNeed.Add(new Need(test2, 1));
+            Population testPop = new Population(10, testNeed, 5);
+            testPop.deltaSpeed = 5;
+            testPop.inventory.Add(new Stock(test1, 15));
+            testPop.inventory.Add(new Stock(test2, 10));
 
-            //Call Funtion
+            //Call Function
+            testPop.CalculateGrowthAndPopulation();
 
             //Asset Outcome
+            Assert.AreEqual(0.25f, testPop.growth);
+
+            Assert.AreEqual(1, testPop.inventory.Count);
         }
     }
 }
